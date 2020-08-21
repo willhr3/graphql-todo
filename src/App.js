828ccf1e69f3b2a625f676332b2cb7956d8e453e
willhr3 <will.hester@r3.com>
2020-08-21 15:29:51 +0100

    Add some css from tachyons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
 
 // Add todos 
 // Toggle todos
 // Delete todos 
 // List todos
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+    done
+    id
+    text
+  }
+}`
+
 function App() {
+  const { data, loading, error } = useQuery(GET_TODOS)
+
+  if (loading) return <div>Loading todos ...</div>
+  if (error) return <div>Error fetching todos </div>
+
   return (
-    <div>
-        App 
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa4 fl-1">
+      <h1 className="f2-1">GraphQL Checklist
+        <span role="img" aria-label="Checkmark"> ✅</span>
+      </h1>
+
+      <form className="mb-3">
+        <input className="pa2 f4 b--dashed" type="text" placeholder="Add your todo" />
+        <button className="pa2 f4 bg-green" type="submit">Create todo</button>
+      </form>
+
+      <div className="flex items-cener justify-left flex-column">
+        {data.todos.map(todo => (
+          <p key={todo.id}>
+            <span className="pointer list pa1 f3">
+              {todo.text}
+            </span>
+            <button className="bg-transparent bn f4">
+              <span className="red">
+                &times;
+                </span>
+            </button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
