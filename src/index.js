c02d238a8f183ffe17e8cd64c4891fa3a65a8ec8
willhr3 <will.hester@r3.com>
2020-08-21 15:03:32 +0100

    Use ApolloProvider as context

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ApolloClient, { gql } from 'apollo-boost';
-// import { ApolloProvide } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks'
 
 const client = new ApolloClient({
   uri: 'https://smart-slug-73.hasura.app/v1/graphql'
 })
 
-client.query({
-  query: gql`
-  query getTodos {
-    todos {
-      done
-      id
-      text
-    }
-  }`
-}).then(data => console.log(data))
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('root'));
